5c2829272a06944b822f21be1630133c709cae25
Ahimas <ahimas.jasan@gmail.com>
2019-12-03 00:39:10 +0530

    initial comit

---
+++ application/app.py
@@ -0,0 +1,84 @@
+#:import utils kivy.utils
+import sys
+import webbrowser
+from kivy.app import App
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.lang import Builder
+from kivy.properties import StringProperty
+from kivy.uix.image import Image
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.button import Button
+from kivy.uix.popup import Popup
+from kivy.uix.label import Label
+from kivy.uix.modalview import ModalView
+import kivy.utils as utils
+from songs import *
+
+Builder.load_file('app_layout.kv')
+
+class FullImage(Image):
+    pass
+
+class CatScreen(Screen):
+    pass
+
+    def changer(self ,*args):
+        pass
+
+    def not_implemented(self,*args):
+
+        popup = Popup(title='Not Implemented',
+                    content=Label(text='will implemented later ........'),
+
+                    size_hint=(None, None), size=(400, 400)
+        )
+        popup.open()
+
+    def about(self,*args):
+        webbrowser.open('https://github.com/Ahimasjason/Kivy-Lyric')
+
+    def exit_screen(self,*args):
+        sys.exit()
+
+class SongsSelectScreen(Screen):
+    pass
+    def changer(self,*args):
+        self.manager.get_screen('lyrics').set_song(args[0])
+        self.manager.transition.direction = 'left'
+        self.manager.current = 'lyrics'
+
+class LyricsScreen(Screen):
+
+    lyric_sc = StringProperty('')
+    song_title = StringProperty('')
+
+    def set_song(self,*args):
+        song = args[0]
+        song_list = song.split('_')
+        song_title = ' '.join(char.upper() for char in song_list )
+        self.lyric_sc =eval(song)
+        self.song_title = song_title
+    def changer(self,*args):
+        '''
+        `meth`
+            To go back from Lyric screen song select screen
+        '''
+        # self.ids['lyric_back_button'].background_color = 1.0, 0.0, 0.0, 1.0
+        self.manager.current ='SelectSong'
+
+class MainApp(App):
+    def __init__(self,**kw):
+        super(MainApp,self).__init__(**kw)
+        self.sm = ScreenManager()
+        self.sm.add_widget(CatScreen(name='Catagory'))
+        self.sm.add_widget(LyricsScreen(name='lyrics'))
+        self.sm.add_widget(SongsSelectScreen(name='SelectSong'))
+
+    def build(self):
+        return self.sm
+
+
+if __name__ == '__main__':
+    app = MainApp()
+    app.run()
---
+++ application/songs.py
@@ -0,0 +1,543 @@
+
+god_rest_ye_merry ='''
+
+God rest ye merry, gentlemen
+Let nothing you dismay
+Remember, Christ, our Saviour
+Was born on Christmas day
+To save us all from Satan's power
+When we were gone astray
+O tidings of comfort and joy
+Comfort and joy
+O tidings of comfort and joy
+
+In Bethlehem, in Israel
+This blessed Babe was born
+And laid within a manger
+Upon this blessed morn
+The which His Mother Mary
+Did nothing take in scorn
+O tidings of comfort and joy
+Comfort and joy
+O tidings of comfort and joy
+
+From God our Heavenly Father
+A blessed Angel came
+And unto certain Shepherds
+Brought tidings of the same
+How that in Bethlehem was born
+The Son of God by Name
+O tidings of comfort and joy
+Comfort and joy
+O tidings of comfort and joy
+
+'''
+
+rudolph_the_red_nosed ='''
+Rudolph, the red-nosed reindeer
+had a very shiny nose
+and if you ever saw it
+you would even say it glows.
+
+All of the other reindeer
+used to laugh and call him names
+They never let poor Rudolph
+play in any reindeer games.
+
+Then one foggy Christmas eve
+Santa came to say:
+"Rudolph with your nose so bright,
+won't you guide my sleigh tonight?"
+
+Then all the reindeer loved him
+as they shouted out with glee,
+Rudolph the red-nosed reindeer,
+you'll go down in history!
+'''
+good_christian_men ='''
+Good Christian men, rejoice,
+With heart and soul, and voice;
+Give ye heed to what we say:
+Jesus Christ is born to-day;
+Ox and ass before him bow,
+And he is in the manger now.
+Christ is born to-day!
+Christ is born to-day!
+Good Christian men, rejoice,
+With heart and soul, and voice;
+Now ye hear of endless bliss:
+Jesus Christ was born for this!
+He hath oped the heavenly door,
+And man is blessed evermore.
+Christ was born for this!
+Christ was born for this!
+Good Christian men, rejoice,
+With heart and soul, and voice;
+Now ye need not fear the grave:
+Jesus Christ was born to save!
+Calls you one and calls you all
+To gain his everlasting hall.
+Christ was born to save!
+Christ was born to save!
+'''
+i_saw_three_ship ='''
+I saw three ships come sailing in
+On Christmas Day, on Christmas Day
+I saw three ships come sailing in
+On Christmas Day in the morning
+Wither sailed those ships all three
+On Christmas Day, on Christmas Day
+Wither sailed those ships all three
+On Christmas Day in the morning
+Oh they sailed into Bethlehem
+On Christmas Day, on Christmas Day
+They sailed into Bethlehem
+On Christmas Day in the morning
+And all the bells on earth shall ring
+On Christmas Day, on Christmas Day
+And all the bells on earth shall ring
+On Christmas Day in the morning
+'''
+vaana_thoothar_senaikal ='''
+வான தூதர் சேனைகள்
+கீதங்களைப் பாடியே
+ஓய்வின்றி துதித்துப் பாலனை வாழ்த்தினர் (2)
+1. ராவேளை மேய்ப்பர்கள் மந்தை காக்கையில்
+தோன்றினர் தூதர்கள் அட்சணமே
+அச்சத்தை நீக்கியே மேய்ப்பரிடம் 
+நற்செய்தி கூறியே மகிழ்வித்தனர்
+சேர்ந்து நாமும் சென்றங்கு
+காண்போம் நம் பாலனை
+– வானதூதர்
+2. பொன் தூபம் வெள்ளைப் போளம் ஏற்றிடுவோம்
+சென்றனர் பாலனை தரிசிக்க
+வான் நட்சத்திரத்தின் ஒளியிலே
+மாட்டுத் தொழுவத்தை அடைந்தனர்
+சேர்ந்து நாமும் சென்றங்கு
+காண்போம் நம் பாலனை
+– வானதூதர்
+
+
+vaana thoothar senaikal
+geethangalaip paatiyae
+oyvinti thuthiththup paalanai vaalththinar (2)
+1. raavaelai maeypparkal manthai kaakkaiyil
+thontinar thootharkal atchanamae
+achchaththai neekkiyae maeypparidam
+narseythi kooriyae makilviththanar
+sernthu naamum sentangu
+kaannpom nam paalanai
+– vaanathoothar
+2. pon thoopam vellaip polam aettiduvom
+sentanar paalanai tharisikka
+vaan natchaththiraththin oliyilae
+maattuth tholuvaththai atainthanar
+sernthu naamum sentangu
+kaannpom nam paalanai
+– vaanathoothar
+'''
+away_in_a_manger ='''
+Away in a manger no crib for a bed
+The little lord jesus laid down his sweet head
+The stars in the bright sky looked down where he lay
+The little lord jesus asleep on the hay.
+
+The cattle are lowing the baby awakes
+But little lord jesus no crying he makes.
+I love you lord jesus; look down from the sky
+And stay by my side until morning is nigh.
+
+Be near me lord jesus i ask you to stay
+Close by me for ever, and love me, i pray.
+Bless all the dear children in your tender care,
+And fit us for heaven, to live with you there.
+'''
+said_the_night='''
+Said the night wind to the little lamb
+Do you see what I see
+Way up in the sky little lamb
+Do you see what I see
+A star, a star
+Dancing in the night
+With a tail as big as a kite
+With a tail as big as a kite
+
+Said the shepherd boy to the mighty king
+Do you know what I know
+In your palace wall mighty king
+Do you know what I know
+A child, a child
+Shivers in the cold
+Let us bring him silver and gold
+Let us bring him silver and gold
+Said the king to the people everywhere
+Listen to what I say
+Pray for peace people everywhere
+Listen to what I say
+The child, the child
+Sleeping in the night
+He will bring us goodness and light
+He will bring us goodness and light
+'''
+it_came_midnight_clear ='''
+It came upon the midnight clear
+That glorious song of old
+From Angels playing near the earth
+To touch their harps of gold
+Peace on the earth could will two men
+From Heaven's all-gracious King
+The world in solemn stillness lay
+To hear the Angels sing
+And I hear them singing
+Sing, I do hear them singing
+The first Noel the Angel did say
+Was to certain poor shepherds in fields as they lay
+In fields where they lay, they keeping their sheep
+On a cold winter's night that was so deep
+
+
+'''
+mary_did_you_konw ='''
+Mary did you know that your baby boy would one day walk on water?
+Mary did you know that your baby boy would save our sons and daughters?
+Did you know that your baby boy has come to make you new?
+This child that you've delivered, will soon deliver you
+Mary did you know that your baby boy will give sight to a blind man?
+Mary did you know that your baby boy will calm a storm with his hand?
+Did you know that your baby boy has walked where angels trod?
+When you kiss your little baby, you kiss the face of God
+The blind will see, the deaf will hear, the dead will live again
+The lame will leap, the dumb will speak, the praises of the lamb
+Mary did you know that your baby boy is Lord of all creation?
+Mary did you know that your baby boy would one day rule the nations?
+Did you know that your baby boy is heaven's perfect lamb?
+That sleeping child you're holding is the great I am
+
+
+
+'''
+chinachiru_sudhane ='''
+சின்னஞ்சிறு சுதனே என்னரும் தவமே
+மன்னர் மன்னவனே உன்னததிருவே
+
+1. காடுண்டு நரிக்கு குழிகளுமுண்டு
+கூடுண்டு பறவைகட்கு
+பாடுண்டு உமக்கு மனிதகுமாரனே
+வீடுண்டோ உந்தனுக்கு
+தாரணி துயர்கள்
+துன்பங்கள் நீங்க
+காரணம் நீரானீரோ
+கோர வெம்பகைகள் பாரச்சுமைகள்
+தீர மருந்தானீரோ – ஆ..ஆ..ஆ
+சின்னஞ்சிறு சுதனே என்னரும் தவமே
+மன்னர் மன்னவனே உன்னததிருவே
+
+Chinnanchiru suthane ennerum thavame
+Mannar mannavane unnathiruve
+
+Kaadundu narikku kuligalumundu
+Koodundu paravaigatku
+Padundu umakku manithakumarane
+Veedundo unthanukku
+Tharani thuyargal
+Thunbangal neenga
+Karanam neeraneero
+Koravem pagaigal paara sumaigal
+Theera marunthaaneero – Aa Aa Aa
+
+Chinnanchiru suthane ennerum thavame
+Mannar mannavane unnathiruve
+
+'''
+a_ray_of_hope ='''
+A ray of hope flitters in the sky
+A tiny star lights up way up high
+All across the land dawns a brand new morn
+This comes to pass when a child is born
+A silent wish sails the seven seas
+The winds have changed whisper in the trees
+And the walls of doubt crumble tossed and torn
+This comes to pass when a child is born
+A rosy hue settles all around
+You've got the feel you're on solid ground
+For a spell or two no-one seems forlorn
+This comes to pass when a child is born
+It's all a dream and illusion now
+It must come true, sometimes soon somehow
+All across the land dawns a brand new morn
+This comes to pass when a child is born
+'''
+merry_christmas ='''
+Feliz Navidad
+Feliz Navidad
+Feliz Navidad
+Prospero año y felicidad
+I want to wish you a Merry Christmas
+I want to wish you a Merry Christmas
+I want to wish you a Merry Christmas
+From the bottom of my heart
+'''
+theva_balan = '''
+தேவ பாலன் பிறந்தீரே
+மனுக் கோலம் எடுத்தீரே
+வானலோகம் துறந்தீரே இயேசுவே
+நீர் வாழ்க வாழ்கவே
+
+மண் மீதினில் மாண்புடனே
+மகிமையாய் உதித்த  மன்னவரே
+வாழ்த்திடுவோம் வணங்கிடுவோம்
+தூய உன் நாமத்தையே
+--தேவ பாலன்
+
+thEva palan piRanthIrE
+manuk kOlam etuththIrE
+vanalOkam thuRanthIrE iyEsuvE
+nIr vazhka vazhkavE
+
+maN mIthinil maNputanE
+makimaiyay uthiththa  mannavarE
+vazhththituvOm vaNangkituvOm
+thUya un namaththaiyE
+
+--thEva palan
+'''
+gentle_mary_laid ='''
+Gentle Mary laid her child lowly in a manger;
+There He lay, the undefiled, to the world a stranger:
+Such a babe in such a place, can He be the Savior?
+Ask the saved of all the race who have found His favor.
+Angels sang about His birth; wise men sought and found Him;
+Heaven’s star shone brightly forth, glory all around Him:
+Shepherds saw the wondrous sight, heard the angels singing;
+All the plains were lit that night, all the hills were ringing.
+Gentle Mary laid her child lowly in a manger;
+He is still the undefiled, but no more a stranger:
+Son of God, of humble birth, beautiful the story;
+Praise His name in all the earth, hail the King of glory!
+'''
+the_first_noel ='''
+The First Noel, the Angels did say
+
+Was to certain poor shepherds in fields as they lay
+
+In fields where they lay keeping their sheep
+
+On a cold winter's night that was so deep
+
+Noel, Noel, Noel, Noel
+
+Born is the King of Israel!
+
+2. They looked up and saw a star
+Shining in the East, beyond them far,
+And to the earth it gave great light,
+And so it continued, both day and night.
+'''
+it_is_most_wonderful ='''
+It's the most wonderful time of the year
+Wit
+it_is_most_wonderful =  the kids jingle belling
+And everyone telling you be of good cheer
+It's the most wonderful time of the year
+It's the hap-happiest season of all
+With those holiday greetings and gay happy meetings
+When friends come to call
+It's the hap-happiest season of all
+
+'''
+mannnnorai_meettidavae ='''
+மண்ணோரை மீட்டிடவே பாரில்
+விண் வேந்தன் மைந்தனாகினார் (2)
+1. தீர்க்கர் உறைத்த வாக்கின்படியே
+மார்க்கம் திறக்க மனிதனானார்
+வாக்கு மாறா தேவ மைந்தன்
+ஏழைக் கன்னி மடியில் உதித்தார்
+மாபுகழ் பாடுவோமே துதி சாற்றுவோமே
+தூதரோடு நாமும் பாடுவோம்
+2. மண்ணில் கொடிய இருள் நீங்க
+மன்னன் ஜீவ ஒளியாய் தோன்றினார்
+விண்ணில் மா ஒளிவிளங்க
+மன்னர் மூவர் தேடி வந்தார்
+மாபுகழ் பாடுவோமே துதி சாற்றுவோமே
+தூதரோடு நாமும் பாடுவோம்
+
+
+mannnnorai meettidavae paaril
+vinn vaenthan mainthanaakinaar (2)
+1. theerkkar uraiththa vaakkinpatiyae
+maarkkam thirakka manithanaanaar
+vaakku maaraa thaeva mainthan
+aelaik kanni matiyil uthiththaar
+maapukal paaduvomae thuthi saattuvomae
+thootharodu naamum paaduvom
+2. mannnnil kotiya irul neenga
+mannan jeeva oliyaay thontinaar
+vinnnnil maa olivilanga
+mannar moovar thaeti vanthaar
+maapukal paaduvomae thuthi saattuvomae
+thootharodu naamum paaduvom
+'''
+namakkoru_paalakan='''
+நமக்கொரு பாலகன் பிறந்தார்
+நமக்கோர் சுதன் கொடுக்கப்பட்டார்
+அவர் இயேசு தெய்வ மைந்தனாம்
+அவர் பாதம் வணங்குவோம்
+உன்னதத்தில் மகிமை
+பூமியில் அமைதி
+என்றும் உண்டாகவே
+1. அதிசயமானவர் அவர் ஆலோசனை கர்த்தா
+வல்லமையுள்ளவர் அமைதி காப்பவர்
+இம்மானுவேலவர் என்றும் நம்மோடிருப்பவர்
+இருளை அகற்றி ஒளியை தருபவர்
+– நமக்கொரு
+2. பாவிகள் நமக்காய் இந்த பாரில் உதித்தவர்
+வான்மகிமைவிட்டு ஏழையாய் வந்தவர்
+தேவாதி தேவனை இந்த ராஜாதி ராஜனை
+ஏகமாய் போற்றியே வாழ்த்திடுவோம் நாமே
+– நமக்கொரு
+
+
+namakkoru paalakan piranthaar
+namakkor suthan kodukkappattar
+avar Yesu theyva mainthanaam
+avar paatham vananguvom
+unnathaththil makimai
+poomiyil amaithi
+entum unndaakavae
+1. athisayamaanavar avar aalosanai karththaa
+vallamaiyullavar amaithi kaappavar
+immaanuvaelavar entum nammotiruppavar
+irulai akatti oliyai tharupavar
+– namakkoru
+2. paavikal namakkaay intha paaril uthiththavar
+vaanmakimaivittu aelaiyaay vanthavar
+thaevaathi thaevanai intha raajaathi raajanai
+aekamaay pottiyae vaalththiduvom naamae
+– namakkoru
+'''
+o_come_o_come ='''
+1 O come, O come, Emmanuel,
+and ransom captive Israel
+who mourns in lonely exile here
+until the Son of God draws near:
+Rejoice, rejoice!
+Emmanuel shall come to you, O Israel.
+
+2 O come, true Wisdom from on high,
+who orders all to mind and eye:
+to us the path of knowledge show
+and teach us in her ways to grow.
+Rejoice, rejoice!...
+3 O come, O come, great Lord of might
+who long ago on Sinai's height
+gave all your tribes the ancient law,
+in cloud and majesty and awe:
+Rejoice, rejoice!...
+
+4 O come, true Branch of Jesse, free
+your children from this tyranny;
+from depths of hell your people save
+to rise victorious from the grave:
+Rejoice, rejoice!...
+
+
+'''
+o_holy_night = '''
+O holy night! The stars are brightly shining
+It is the night of our dear Savior's birth
+Long lay the world in sin and error pining
+'Til He appears and the Soul felt its worth
+A thrill of hope the weary world rejoices
+For yonder breaks a new and glorious morn
+Fall on your knees; O hear the angel voices!
+O night divine, O night when Christ was born
+O night, O holy night, O night divine!
+Truly He taught us to love one another
+His law is love and His gospel is peace
+Chains shall He break for the slave is our brother
+And in His name, all oppression shall cease
+Sweet hymns of joy in grateful chorus raise we
+Let all within us praise His holy name
+Christ is the Lord! Oh, praise His Name forever
+His power and glory evermore proclaim
+Oh, night divine, oh, night when Christ was born
+Fall on your knees! Oh, hear the angel voices
+Oh, night divine 
+Oh, night when Christ was born
+Oh, night divine, oh, night when Christ was born
+
+'''
+come_thy_told_me ='''
+Come thy told me Pa rum pum pum-pum
+A newborn King to see Pa rum pum pum-pum
+Our finest gifts we bring Pa rum pum pum-pum
+To lay before the king Pa rum pum pum-pum
+Rum pum pum-pum Rum pum pum-pum
+So to honor Him Pa rum pum pum-pum
+When we come
+
+Little baby Pa rum pum pum-pum
+I am a poor boy too Pa rum pum pum-pum
+I have no gift to bring Pa rum pum pum-pum
+That's fit to give our King Pa rum pum pum-pum
+Rum pum pum-pum Rum pum pum-pum
+Shall I play for you Pa rum pum pum-pum
+On my drum
+
+Mary nodded Pa rum pum pum-pum
+The ox and lamb kept time Pa rum pum pum-pum
+I played my drum for Him Pa rum pum pum-pum
+I played my best for Him Pa rum pum pum-pum
+Rum pum pum-pum Rum pum pum-pum
+Then He smiled at me Pa rum pum pum-pum
+Me and my drum
+
+'''
+mary_boy_child ='''
+Mary's boy child Jesus Christ, was born on Christmas Day
+And man will live for evermore, because of Christmas Day
+Long time ago in Bethlehem, so the Holy Bible said
+Mary's boy child Jesus Christ, was born on Christmas Day
+Hark, now hear the angels sing, a king was born today
+And man will live for evermore, because of Christmas Day
+Mary's boy child Jesus Christ, was born on Christmas Day
+While shepherds watch their flocks by night
+They see a bright new shining star
+They hear a choir sing a song, the music seemed to come from afar
+Hark, now hear the angels sing, a king was born today
+And man will live for evermore, because of Christmas Day
+
+'''
+snow_is_falling ='''
+Snow is fallin' All around me
+Children playin' Having fun
+It's the season Love and understanding
+Merry Christmas everyone
+Time for parties And celebrations
+People dancin' All night long
+Time for presents And exchanging wishes
+Time for singing Christmas songs
+We're gonna have a party tonight
+I'm gonna find that friend
+Underneath the mistletoe
+We'll wish by candle light
+
+'''
+jingle_bell ='''
+Jingle bell, jingle bell, jingle bell rock
+Jingle bells swing and jingle bells ring
+Snowing and blowing up bushels of fun
+Now the jingle hop has begun
+Jingle bell, jingle bell, jingle bell rock
+Jingle bells chime in jingle bell time
+Dancing and prancing in Jingle Bell Square
+In the frosty air
+What a bright time, it's the right time
+To rock the night away
+Jingle bell time is a swell time
+To go gliding in a one-horse sleigh
+Giddy-up jingle horse, pick up your feet
+Jingle around the clock
+Mix and a-mingle in the jingling feet
+That's the jingle bell rock
+'''
